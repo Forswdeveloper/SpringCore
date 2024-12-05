0dbe79d9fcbf29fe520fd36f9bdac0216625e0e6
jdi0901 <jdi0901@naver.com>
2024-12-05 22:02:52 +0900

    AppConfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -14,11 +16,20 @@ public class AppConfig {
     // 클라이언트 입장( memberServiceImpl 입장에서 보면 마치 외부에서 주입해주는 것 같다고 해서 DI(Dependency Injection) 의존관계주입 / 의존성 주입이라고한다.
     //역할의 구분
     //생성자 주입
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();  // -> 변경 필요 시 수정 부분
+    }
+
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());  //역할이 명시  -> 역할과 구현 클래스의 구분. 중복 제거
+    }
+
+    public DiscountPolicy discountPolicy(){
+        //return new FixDiscountPolicy(); // -> 변경 필요 시 수정 부분
+        return new RateDiscountPolicy();
     }
 }
