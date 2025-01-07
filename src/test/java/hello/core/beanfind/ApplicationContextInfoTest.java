0e20668aa87e6db4f13edb652df2397362b87ff3
jdi0901 <jdi0901@naver.com>
2025-01-08 00:14:44 +0900

    스프링빈 상속조회 추가

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -29,9 +29,14 @@ public class ApplicationContextInfoTest {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
 
             //Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
-            //Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
             //내가 등록한 빈들
             if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " object = " + bean);
+            }
+
+            //Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_INFRASTRUCTURE){
                 Object bean = ac.getBean(beanDefinitionName);
                 System.out.println("beanDefinitionName = " + beanDefinitionName + " object = " + bean);
             }
